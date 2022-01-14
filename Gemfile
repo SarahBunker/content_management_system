38a945982a1330469f1aaca97397fc4d8a1e7549
Sarah Bunker <ChemEnCoder@gmail.com>
2022-01-14 20:18:56 +0000

    setting up gemfile and basic sinatra route

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "http://rubygems.org"
+
+ruby "3.1.0"
+
+gem "erubis"
+gem "sinatra", "1.4.7"
+gem "sinatra-contrib"
+
+gem 'webrick'
+#webrick?
---
+++ cms.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sinatra/content_for'
+require 'tilt/erubis'
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+get '/' do
+  "Hello World"
+end

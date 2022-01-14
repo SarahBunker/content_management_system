8db5e2b006c4850f1a47721f0d032f5d4c933ae9
Sarah Bunker <ChemEnCoder@gmail.com>
2022-01-14 23:48:36 +0000

    changing the home page to a file index page. Accessing the directory and displaying contents.

--- cms.rb
+++ cms.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/reloader' # if development?
 require 'sinatra/content_for'
 require 'tilt/erubis'
 
@@ -10,6 +10,12 @@ configure do
   set :session_secret, 'secret'
 end
 
+root = File.expand_path("..", __FILE__)
+
 get '/' do
-  "Getting started."
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+
+  erb :index
 end
